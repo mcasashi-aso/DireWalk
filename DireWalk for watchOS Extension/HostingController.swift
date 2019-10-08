7bb1d11c5e2224bea57626a69424245e9a271dec
Masashi Aso <maashi.apple.japan@icloud.com>
2019-10-08 14:22:15 +0900

    Im starting watchOS app

---
+++ DireWalk for watchOS Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  DireWalk for watchOS Extension
+//
+//  Created by Masashi Aso on 2019/10/08.
+//  Copyright © 2019 麻生昌志. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ DireWalk for watchOS Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  DireWalk for watchOS Extension
+//
+//  Created by Masashi Aso on 2019/10/08.
+//  Copyright © 2019 麻生昌志. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
